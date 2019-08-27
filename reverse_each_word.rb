8fcd84f5d182e561541d24ebfca95b1f4f860c9b
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 19:34:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
 def reverse_each_word(sentence)
  sentence.split(" ").collect { |word| word.reverse }.join(" ")
 end
+
+# sentence_array = sentence.split(" ")
+# sentence_array_reversed = [] 
+ 
+# sentence_array.each do |word|
+#   sentence_array_reversed << word.reverse
+# end
+ 
